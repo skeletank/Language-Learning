575892d9ad45aad4ff6c0db81490da4a395268cf
skeletank <skeletank@gmail.com>
2014-05-08 22:21:18 -0500

    Adding conjugation functionality

--- MRB.LanguageLearning/MainWindow.xaml.cs
+++ MRB.LanguageLearning/MainWindow.xaml.cs
@@ -24,7 +24,8 @@ namespace MRB.LanguageLearning
         #region Fields
 
         private DatabaseController _databaseController = new DatabaseController();
-        private Verb_Regular _currentVerb;
+        private Verb_Regular _vocab_CurrentVerb;
+        private Verb_Regular _conjugation_CurrentVerb;
         private int _questionsAsked = 0;
         private int _questionsCorrect = 0;
 
@@ -36,46 +37,124 @@ namespace MRB.LanguageLearning
         {
             InitializeComponent();
 
-            ResetWithNewVerb();
+            Vocab_ResetWithNewVerb();
+            Conjugation_ResetWithNewVerb();
         }
 
         #endregion
 
         #region Event Handlers
 
-        private void NewVerbButton_Click(object sender, RoutedEventArgs e)
+        private void Vocab_NewVerbButton_Click(object sender, RoutedEventArgs e)
         {
-            ResetWithNewVerb();
+            Vocab_ResetWithNewVerb();
         }
 
-        private void ShowCorrectAnswerButton_Click(object sender, RoutedEventArgs e)
+        private void Vocab_ShowCorrectAnswerButton_Click(object sender, RoutedEventArgs e)
         {
             GuessVerbTextBox.Foreground = Brushes.Blue;
-            GuessVerbTextBox.Text = _currentVerb.EnglishDefinition;
+            GuessVerbTextBox.Text = _vocab_CurrentVerb.EnglishDefinition;
         }
 
-        private void VerifyVerbButton_Click(object sender, RoutedEventArgs e)
+        private void Vocab_VerifyVerbButton_Click(object sender, RoutedEventArgs e)
         {
-            bool isCorrect = String.Compare(_currentVerb.EnglishDefinition, GuessVerbTextBox.Text, true) == 0;
+            bool isCorrect = String.Compare(_vocab_CurrentVerb.EnglishDefinition, GuessVerbTextBox.Text, true) == 0;
 
             if (isCorrect)
                 GuessVerbTextBox.Foreground = Brushes.Green;
             else
                 GuessVerbTextBox.Foreground = Brushes.Red;
 
-            UpdateScore(isCorrect);
+            Vocab_UpdateScore(isCorrect);
         }
 
-        private void ResetScoreButton_Click(object sender, RoutedEventArgs e)
+        private void Vocab_ResetScoreButton_Click(object sender, RoutedEventArgs e)
         {
+            ScoreValueLabel.Content = String.Empty;
+        }
 
+        private void Conjugation_NewVerbButton_Click(object sender, RoutedEventArgs e)
+        {
+            Conjugation_ResetWithNewVerb();
+        }
+
+        private void Conjugation_ShowCorrectAnswerButton_Click(object sender, RoutedEventArgs e)
+        {
+            FirstPersonSingularTextBox.Foreground = Brushes.Blue;
+            FirstPersonSingularTextBox.Text = _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_1stPerson_Singular_Active_Ending;
+
+            FirstPersonPluralTextBox.Foreground = Brushes.Blue;
+            FirstPersonPluralTextBox.Text = _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_1stPerson_Plural_Active_Ending;
+
+            SecondPersonSingularTextBox.Foreground = Brushes.Blue;
+            SecondPersonSingularTextBox.Text = _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_2ndPerson_Singular_Active_Ending;
+
+            SecondPersonPluralTextBox.Foreground = Brushes.Blue;
+            SecondPersonPluralTextBox.Text = _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_2ndPerson_Plural_Active_Ending;
+
+            ThirdPersonSingularTextBox.Foreground = Brushes.Blue;
+            ThirdPersonSingularTextBox.Text = _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_3rdPerson_Singular_Active_Ending;
+
+            ThirdPersonPluralTextBox.Foreground = Brushes.Blue;
+            ThirdPersonPluralTextBox.Text = _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_3rdPerson_Plural_Active_Ending;
+        }
+
+        private void Conjugation_VerifyVerbButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isFirstPersonSingularCorrect = 
+                FirstPersonSingularTextBox.Text == _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_1stPerson_Singular_Active_Ending;
+
+            if (isFirstPersonSingularCorrect)
+                FirstPersonSingularTextBox.Foreground = Brushes.Green;
+            else
+                FirstPersonSingularTextBox.Foreground = Brushes.Red;
+
+            bool isFirstPersonPluralCorrect =
+                FirstPersonPluralTextBox.Text == _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_1stPerson_Plural_Active_Ending;
+
+            if (isFirstPersonPluralCorrect)
+                FirstPersonPluralTextBox.Foreground = Brushes.Green;
+            else
+                FirstPersonPluralTextBox.Foreground = Brushes.Red;
+
+            bool isSecondPersonSingularCorrect =
+                SecondPersonSingularTextBox.Text == _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_2ndPerson_Singular_Active_Ending;
+
+            if (isSecondPersonSingularCorrect)
+                SecondPersonSingularTextBox.Foreground = Brushes.Green;
+            else
+                SecondPersonSingularTextBox.Foreground = Brushes.Red;
+
+            bool isSecondPersonPluralCorrect =
+                SecondPersonPluralTextBox.Text == _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_2ndPerson_Plural_Active_Ending;
+
+            if (isSecondPersonPluralCorrect)
+                SecondPersonPluralTextBox.Foreground = Brushes.Green;
+            else
+                SecondPersonPluralTextBox.Foreground = Brushes.Red;
+
+            bool isThirdPersonSingularCorrect =
+                ThirdPersonSingularTextBox.Text == _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_3rdPerson_Singular_Active_Ending;
+
+            if (isThirdPersonSingularCorrect)
+                ThirdPersonSingularTextBox.Foreground = Brushes.Green;
+            else
+                ThirdPersonSingularTextBox.Foreground = Brushes.Red;
+
+            bool isThirdPersonPluralCorrect =
+                ThirdPersonPluralTextBox.Text == _conjugation_CurrentVerb.Root + _conjugation_CurrentVerb.Conjugation.Present_3rdPerson_Plural_Active_Ending;
+
+            if (isThirdPersonPluralCorrect)
+                ThirdPersonPluralTextBox.Foreground = Brushes.Green;
+            else
+                ThirdPersonPluralTextBox.Foreground = Brushes.Red;
         }
 
         #endregion
 
         #region Methods
 
-        private void UpdateScore(bool isCorrect)
+        private void Vocab_UpdateScore(bool isCorrect)
         {
             if (isCorrect)
                 _questionsCorrect++;
@@ -85,14 +164,39 @@ namespace MRB.LanguageLearning
             ScoreValueLabel.Content = String.Format("{0} of {1}", _questionsCorrect, _questionsAsked);
         }
 
-        private void ResetWithNewVerb()
+        private void Vocab_ResetWithNewVerb()
         {
-            _currentVerb = _databaseController.GetRandomVerb();
+            _vocab_CurrentVerb = _databaseController.GetRandomVerb();
 
             GuessVerbTextBox.Text = String.Empty;
             GuessVerbTextBox.Foreground = Brushes.Black;
 
-            GivenVerbLabel.Content = _currentVerb.Infinitive;
+            GivenVerbLabel.Content = _vocab_CurrentVerb.Infinitive;
+        }
+
+        private void Conjugation_ResetWithNewVerb()
+        {
+            _conjugation_CurrentVerb = _databaseController.GetRandomVerb();
+
+            FirstPersonSingularTextBox.Text = String.Empty;
+            FirstPersonSingularTextBox.Foreground = Brushes.Black;
+
+            FirstPersonPluralTextBox.Text = String.Empty;
+            FirstPersonPluralTextBox.Foreground = Brushes.Black;
+
+            SecondPersonSingularTextBox.Text = String.Empty;
+            SecondPersonSingularTextBox.Foreground = Brushes.Black;
+
+            SecondPersonPluralTextBox.Text = String.Empty;
+            SecondPersonPluralTextBox.Foreground = Brushes.Black;
+
+            ThirdPersonSingularTextBox.Text = String.Empty;
+            ThirdPersonSingularTextBox.Foreground = Brushes.Black;
+
+            ThirdPersonPluralTextBox.Text = String.Empty;
+            ThirdPersonPluralTextBox.Foreground = Brushes.Black;
+
+            VerbToConjugateLabel.Content = _conjugation_CurrentVerb.Infinitive;
         }
 
         #endregion
