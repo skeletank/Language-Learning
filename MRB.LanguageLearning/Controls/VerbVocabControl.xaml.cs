fecc198c0831b05964fac9b810651f9b6f99825f
skeletank <skeletank@gmail.com>
2014-05-19 12:11:50 -0500

    Ability to move to the next verb w/o using mouse

--- MRB.LanguageLearning/Controls/VerbVocabControl.xaml.cs
+++ MRB.LanguageLearning/Controls/VerbVocabControl.xaml.cs
@@ -17,107 +17,130 @@ using System.Windows.Shapes;
 
 namespace MRB.LanguageLearning.Controls
 {
-    /// <summary>
-    /// Interaction logic for VerbVocabControl.xaml
-    /// </summary>
-    public partial class VerbVocabControl : UserControl
+  /// <summary>
+  /// Interaction logic for VerbVocabControl.xaml
+  /// </summary>
+  public partial class VerbVocabControl : UserControl
+  {
+    #region Fields
+
+    private DatabaseController _databaseController = new DatabaseController();
+
+    private Verb_Regular _currentVerb;
+
+    private int _questionsAsked = 0;
+    private int _questionsCorrect = 0;
+
+    private bool _hasBeenVerified = false;
+
+    #endregion
+
+    #region Constructors
+
+    public VerbVocabControl()
     {
-        #region Fields
+      InitializeComponent();
 
-        private DatabaseController _databaseController = new DatabaseController();
+      if (!DesignerProperties.GetIsInDesignMode(this))
+      {
+        ConjugationComboBox.ItemsSource = _databaseController.GetAllConjugations();
+        ConjugationComboBox.DisplayMemberPath = "Name";
 
-        private Verb_Regular _currentVerb;
+        ConjugationComboBox.SelectedIndex = 0;
 
-        private int _questionsAsked = 0;
-        private int _questionsCorrect = 0;
-
-        #endregion
-
-        #region Constructors
-
-        public VerbVocabControl()
-        {
-            InitializeComponent();
-
-            if (!DesignerProperties.GetIsInDesignMode(this))
-            {
-                ConjugationComboBox.ItemsSource = _databaseController.GetAllConjugations();
-                ConjugationComboBox.DisplayMemberPath = "Name";
-
-                ConjugationComboBox.SelectedIndex = 0;
-
-                ResetWithNewVerb();
-            }
-        }
-
-        #endregion
-
-        #region Event Handlers
-
-        private void NewVerbButton_Click(object sender, RoutedEventArgs e)
-        {
-            CorrectVerbLabel.Content = String.Empty;
-            VerifyVerbButton.IsEnabled = true;
-
-            ResetWithNewVerb();
-        }
-
-        private void VerifyVerbButton_Click(object sender, RoutedEventArgs e)
-        {
-            bool isCorrect = String.Compare(_currentVerb.EnglishDefinition, GuessVerbTextBox.Text, true) == 0;
-
-            if (isCorrect)
-                GuessVerbTextBox.Foreground = Brushes.Green;
-            else
-                GuessVerbTextBox.Foreground = Brushes.Red;
-
-            UpdateScore(_currentVerb, isCorrect);
-
-            CorrectVerbLabel.Content = _currentVerb.EnglishDefinition;
-
-            VerifyVerbButton.IsEnabled = false;
-        }
-
-        private void ResetScoreButton_Click(object sender, RoutedEventArgs e)
-        {
-            ScoreValueLabel.Content = String.Empty;
-        }
-
-        private void ConjugationComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ResetWithNewVerb();
-        }
-
-        #endregion
-
-        #region Methods
-
-        private void UpdateScore(Verb_Regular verb, bool isCorrect)
-        {
-            _databaseController.UpdateVerbSuccessRate(verb, isCorrect);
-
-            if (isCorrect)
-            {
-                _questionsCorrect++;
-            }
-
-            _questionsAsked++;
-
-            ScoreValueLabel.Content = String.Format("{0} of {1}", _questionsCorrect, _questionsAsked);
-        }
-
-        private void ResetWithNewVerb()
-        {
-            Conjugation selectedConjugation = (Conjugation)ConjugationComboBox.SelectedItem;
-
-            _currentVerb = _databaseController.GetRandomVerb(selectedConjugation.Id);
-
-            GuessVerbTextBox.Text = String.Empty;
-            GuessVerbTextBox.Foreground = Brushes.Black;
-
-            GivenVerbLabel.Content = _currentVerb.Infinitive;
-        }
-
-        #endregion
+        ResetWithNewVerb();
+      }
     }
+
+    #endregion
+
+    #region Event Handlers
+
+    private void NewVerbButton_Click(object sender, RoutedEventArgs e)
+    {
+      ResetWithNewVerb();
+    }
+
+    private void VerifyVerbButton_Click(object sender, RoutedEventArgs e)
+    {
+      VerifyAnswer();
+    }
+
+    private void ResetScoreButton_Click(object sender, RoutedEventArgs e)
+    {
+      ScoreValueLabel.Content = String.Empty;
+    }
+
+    private void ConjugationComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      ResetWithNewVerb();
+    }
+
+    private void GuessVerbTextBox_KeyUp(object sender, KeyEventArgs e)
+    {
+      if (e.Key == Key.Enter)
+      {
+        if (_hasBeenVerified)
+          ResetWithNewVerb();
+        else
+          VerifyAnswer();
+      }
+    }
+
+    #endregion
+
+    #region Methods
+
+    private void UpdateScore(Verb_Regular verb, bool isCorrect)
+    {
+      _databaseController.UpdateVerbSuccessRate(verb, isCorrect);
+
+      if (isCorrect)
+      {
+        _questionsCorrect++;
+      }
+
+      _questionsAsked++;
+
+      ScoreValueLabel.Content = String.Format("{0} of {1}", _questionsCorrect, _questionsAsked);
+    }
+
+    private void ResetWithNewVerb()
+    {
+      Conjugation selectedConjugation = (Conjugation)ConjugationComboBox.SelectedItem;
+
+      _currentVerb = _databaseController.GetRandomVerb(selectedConjugation.Id);
+
+      GivenVerbLabel.Content = _currentVerb.Infinitive;
+
+      GuessVerbTextBox.Text = String.Empty;
+      GuessVerbTextBox.Foreground = Brushes.Black;
+
+      CorrectVerbLabel.Content = String.Empty;
+
+      VerifyVerbButton.IsEnabled = true;
+
+      _hasBeenVerified = false;
+    }
+
+    private void VerifyAnswer()
+    {
+      bool isCorrect = String.Compare(_currentVerb.EnglishDefinition, GuessVerbTextBox.Text, true) == 0;
+
+      if (isCorrect)
+        GuessVerbTextBox.Foreground = Brushes.Green;
+      else
+        GuessVerbTextBox.Foreground = Brushes.Red;
+
+      UpdateScore(_currentVerb, isCorrect);
+
+      CorrectVerbLabel.Content = _currentVerb.EnglishDefinition;
+
+      VerifyVerbButton.IsEnabled = false;
+
+      _hasBeenVerified = true;
+    }
+
+    #endregion
+  }
 }
