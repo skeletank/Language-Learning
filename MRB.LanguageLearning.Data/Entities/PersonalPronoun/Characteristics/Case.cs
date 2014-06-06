710e6e42471c17bb8f18f693ec4dd5ee3d8281e2
skeletank <skeletank@gmail.com>
2014-06-05 21:35:20 -0500

    Preparing for personal pronoun practice

---
+++ MRB.LanguageLearning.Data/Entities/PersonalPronoun/Characteristics/Case.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRB.LanguageLearning.Data.Entities.PersonalPronoun.Characteristics
+{
+  public enum Case
+  {
+    Nominative,
+    Genitive,
+    Dative,
+    Accusative,
+    Ablative
+  }
+}
---
+++ MRB.LanguageLearning.Data/Entities/PersonalPronoun/Characteristics/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRB.LanguageLearning.Data.Entities.PersonalPronoun.Characteristics
+{
+  public enum Number
+  {
+    Singular,
+    Plural
+  }
+}
